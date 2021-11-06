8bb19e2be5dbb0b5da62f7302afde90954448165
JirongKim <munja__@naver.com>
2021-11-06 18:12:15 +0900

    Adding Random word REF(axios, Random word api)

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const answer = document.querySelector('.word');
 const scoreDisplay = document.querySelector('.score');
 const timeDisplay = document.querySelector('.time');
 const buttonDisplay = document.querySelector('.loading');
-const GAMETIME = 10;
+const GAMETIME = 5;
 
 let score = 0;
 let time = GAMETIME;
@@ -11,39 +11,56 @@ let isPlaying = false;
 let timer;
 let words = [];
 
-init();
-
-function init(){
-  getWords()
-}
-
-function getWords(){
-  words = ['Hello', 'StarBucks', 'Coffee', 'Delicious'];
-}
-
 buttonChange('게임시작');
 
+// 게임시작 버튼을 눌렀을 때
 function run(){
   if(!isPlaying){
+    getWords();
     isPlaying = true;
     time = GAMETIME;
+    score = 0;
+    scoreDisplay.innerText = score;
     timer = setInterval(countDown, 1000);
-    console.log(timer);
     buttonChange('게임종료');
   }
   else {
+    isPlaying = false;
+    timeDisplay.innerText = 0;
     clearInterval(timer);
     buttonChange('게임시작');
   }
 }
 
+// 단어를 무작위로 받아오는 함수
+function getWords(){
+  buttonChange('게임을 불러오는중...');
+  axios.get('https://random-word-api.herokuapp.com/word?number=1000')
+  .then(function (response) {
+    response.data.forEach((word) => {
+      if(word.length < 10){
+        words.push(word);
+      }
+    });
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
+  answer.innerText = words[Math.floor(Math.random() * words.length)];
+}
+
+// enterKey가 입력됐을 때
 function enterKey(){
   if(window.event.keyCode == 13)
   {
     // console.log(userText.value);
     // console.log(answer.innerText);
-    if(userText.value == answer.innerText)
+    if(userText.value == answer.innerText && isPlaying)
     {
+      answer.innerText = words[Math.floor(Math.random() * words.length)];
+      time = GAMETIME;
+      timeDisplay.innerText = time
       score++;
       scoreDisplay.innerText = score;
     }
@@ -51,21 +68,18 @@ function enterKey(){
   }
 }
 
-function checkStatus(){
-  if(!isPlaying){
-    buttonChange('게임시작');
-  }
-}
-
+// 1초마다 countDown 됨.
 function countDown(){
   time > 0 ? time-- : isPlaying = false;
   if(!isPlaying){
     clearInterval(timer);
+    buttonChange('게임시작');
   }
   timeDisplay.innerText = time
 }
 
+// buttonChange 함수
 function buttonChange(text){
   buttonDisplay.value = text;
-  text == '게임시작' ? buttonDisplay.classList.remove('loading') : buttonDisplay.classList.add('loading');
+  text != '게임을 불러오는중...' ? buttonDisplay.classList.remove('loading') : buttonDisplay.classList.add('loading');
 }
